7c9c77e438d112881cf250f2025e6b92d6345ca5
LEUNSU <lesu002@naver.com>
2024-02-01 14:19:00 +0900

    파이썬 기초 일단 완료

--- day04/test28_fileio.py
+++ day04/test28_fileio.py
@@ -3,9 +3,9 @@
 
 # mode : a(ppend: 내용추가) r(ead: 파일읽기) w(rite: 파일쓰기)
 # encoding : cp949(euc-kr : 한글), utf-8(만국공통어)
-f = open('sample.txt', mode= 'w', encoding= 'utf-8')
+f = open(r'sample.txt', mode= 'w', encoding= 'utf-8')
 # 뭔가를 한다. write()에서 엔터를 추가하려면 마지막에 \n 추가
-f.write('안녕하세요. 우리는 IoT개발자 과정입니다.') # mode a, w
+f.write('안녕하세요. 우리는 IoT개발자 과정입니다.\n') # mode a, w
 f.write('반갑습니다.\n')
 
 f.close() # 파일은 무조건 마지막에 닫는다
