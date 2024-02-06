60a67324baedee8c86919db8a64959db2782dadb
LEUNSU <lesu002@naver.com>
2024-02-06 17:22:49 +0900

    7일차 완료

--- day07/test41_pyqt.py
+++ day07/test41_pyqt.py
@@ -16,23 +16,28 @@ class WinApp(QWidget):
     def initUI(self):
         # 이미지 추가 .scaledToWidth() 큰 해상도를 w800으로 고정
         pixmap = QPixmap('./images/puppy.jpg').scaledToWidth(800)
-
         lblImage = QLabel(self)
         lblImage.setPixmap(pixmap)
+
         lblSize = QLabel(self)
+        lblSize.setFont(QFont('NanumGothicCoding, 20')) # 폰트와 폰트사이즈
+        lblSize.setStyleSheet('Color: red;')
         lblSize.setText(f'{pixmap.width()} x {pixmap.height()}') # puppy.jpg의 width x height
+        lblSize.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter) # 가로중앙정렬 | 세로중앙정렬
 
         vbox = QVBoxLayout(self) # QtDesigner VerticalLayout 위젯 생성
         vbox.addWidget(lblImage) # VL에 위젯 추가
         vbox.addWidget(lblSize)
         self.setLayout(vbox) # Form에 VL 추가와 동일
-        
+
         self.setWindowIcon(QIcon('./images/iot.png'))       
         self.setWindowTitle('이미지 뷰어')
-        self.setGeometry(300,300,300,300) 
-        self.setCenter()
+        rect = QRect(300,300,300,300) # x, y, w, h
+        self.setGeometry(rect) # 같은 이름의 함수를 여러 개 선언해 놓고 입맛에 맞게 골라쓰는(오버로딩)
+        # self.setGeometry(300,300,300,300) 
         self.show() # showFullscreen() 모니터를 꽉 채워서 출력
-    
+        self.setCenter()
+        
     def setCenter(self): ## 윈앱을 화면 정중앙에 위치
         gm = self.frameGeometry() # 윈앱 자신 위치값
         cp = QDesktopWidget().availableGeometry().center() # 모니터의 정중앙 값
@@ -41,7 +46,7 @@ class WinApp(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    screen_rect = app.desktop().screenGeometry()
+    screen_rect = app.desktop().screenGeometry() # 모니터 해상도 구하는법
     width, height = screen_rect.width(), screen_rect.height()
     print(width, 'x', height) # 활용도 높음
     ins = WinApp()
